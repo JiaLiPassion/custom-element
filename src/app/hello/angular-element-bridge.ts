ef360f986500749735a174cce4f16bba54f6156e
JiaLi.Passion <JiaLi.Passion@gmail.com>
2018-07-09 14:48:31 +0900

    add custom Events sample

--- src/app/hello/angular-element-bridge.ts
+++ src/app/hello/angular-element-bridge.ts
@@ -1,5 +1,7 @@
-import {Injector, ComponentFactory, ComponentFactoryResolver, ComponentRef, ChangeDetectorRef, ApplicationRef} from '@angular/core';
-
+import {ApplicationRef, ChangeDetectorRef, ComponentFactory, ComponentFactoryResolver,
+   ComponentRef, EventEmitter, Injector} from '@angular/core';
+import { Observable, merge, Subscription } from 'rxjs';
+import {map} from 'rxjs/operators';
 import {HelloComponent} from './hello.component';
 
 export class AngularCustomElementBridge {
@@ -8,35 +10,63 @@ export class AngularCustomElementBridge {
   initialInputValues = {};
   applicationRef: ApplicationRef;
   changeDetectorRef: ChangeDetectorRef;
+  outputEvents: Observable<any>;
+  ngElementEventsSubscription: Subscription;
 
   constructor(private injector: Injector, private component) {
     this.changeDetectorRef = this.injector.get(ChangeDetectorRef);
   }
 
   prepare() {
-    this.componentFactory = this.injector.get(ComponentFactoryResolver).resolveComponentFactory(this.component);
+    this.componentFactory = this.injector.get(ComponentFactoryResolver)
+                                .resolveComponentFactory(this.component);
   }
 
   initComponent(element: HTMLElement) {
     // first we need an componentInjector to initialize the component.
-    // here the injector is from outside of Custom Element, user can register some of their own
-    // providers in it.
+    // here the injector is from outside of Custom Element, user can register
+    // some of their own providers in it.
     const componentInjector = Injector.create([], this.injector);
     // analyze projectable nodes from element
     // for example:
     // <app-hello>
     //   <div class="inside">some projection content</div>
     // </app-hello>
-    // const projectableNodes = this.injector.get(ComponentFactoryResolver).resolveComponentFactory(this.component);
+    // const projectableNodes =
+    // this.injector.get(ComponentFactoryResolver).resolveComponentFactory(this.component);
     // Here we got all we need, we will initialize the component
-    this.componentRef = this.componentFactory.create(componentInjector, null, element);
+    this.componentRef =
+        this.componentFactory.create(componentInjector, null, element);
 
-    // Then we need to check whether we need to initialize value of component's input
-    // the case is, before Angular Element is loaded, user may already set element's property.
-    // those values will be kept in an initialInputValues map.
-    this.componentFactory.inputs.forEach(prop => this.componentRef.instance[prop.propName] = this.initialInputValues[prop.propName]);
+    // Then we need to check whether we need to initialize value of component's
+    // input the case is, before Angular Element is loaded, user may already set
+    // element's property. those values will be kept in an initialInputValues
+    // map.
+    this.componentFactory.inputs.forEach(
+        prop => this.componentRef.instance[prop.propName] =
+            this.initialInputValues[prop.propName]);
 
-    // then we will trigger a change detection so the component will be rendered in next tick.
+    // subscribe to event emitters of Angular Component and dispatch Custom
+    // Events
+    const eventEmitters =
+        this.componentFactory.outputs.map(({propName, templateName}) => {
+          const emitter = (this.componentRef.instance as any)[propName] as
+              EventEmitter<any>;
+          return emitter.pipe(
+              map((value: any) => ({name: templateName, value})));
+        });
+    // merge all those output event emitter to a single stream.
+    this.outputEvents = merge(...eventEmitters);
+    // Listen for events from the merged stream and dispatch them as custom
+    // events
+    this.ngElementEventsSubscription = this.outputEvents.subscribe(e => {
+      const customEvent = document.createEvent('CustomEvent');
+      customEvent.initCustomEvent(e.name, false, false, e.value);
+      element.dispatchEvent(customEvent);
+    });
+
+    // then we will trigger a change detection so the component will be rendered
+    // in next tick.
     this.changeDetectorRef.detectChanges();
     this.applicationRef = this.injector.get(ApplicationRef);
 
@@ -46,6 +76,7 @@ export class AngularCustomElementBridge {
 
   destroyComponent() {
     this.componentRef.destroy();
+    this.ngElementEventsSubscription.unsubscribe();
   }
 
   setInput(propName: string, value: string) {
@@ -53,10 +84,10 @@ export class AngularCustomElementBridge {
       this.initialInputValues[propName] = value;
       return;
     }
-    if (this.componentRef[propName] === value) {
+    if (this.componentRef.instance[propName] === value) {
       return;
     }
-    this.componentRef[propName] = value;
+    this.componentRef.instance[propName] = value;
     this.changeDetectorRef.detectChanges();
   }
 }
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -7,5 +7,10 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+  @Output() highlight: EventEmitter<string> = new EventEmitter();
   constructor() { }
+
+  click() {
+    this.highlight.emit('highlighted');
+  }
 }
